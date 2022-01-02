ce187c58253a671e8a3f01dc905208b2eea26744
trust oriabure <trust4u@hotmail.it>
2022-01-02 16:23:46 +0100

    save

--- resources/js/components/menu.js
+++ resources/js/components/menu.js
@@ -1,11 +1,16 @@
-export default function (){
-const menuItems = document.querySelectorAll('.menu__item');
-const menu = document.querySelector('.menu');
-menu.addEventListener('click', ()=>{
-menuItems[0].classList.toggle('menu--top');
-menuItems[1].classList.toggle('menu--center');
-menuItems[2].classList.toggle('menu--bottom')
-})
+export default function () {
+    const menuItems = document.querySelectorAll(".menu__item");
+    const menu = document.querySelectorAll(".menu");
+    const nav = document.querySelector(".m-nav");
+    menu.forEach((m) => {
+        m.addEventListener("click", () => {
+            menuItems[0].classList.toggle("menu--top");
+            menuItems[1].classList.toggle("menu--center");
+            menuItems[2].classList.toggle("menu--bottom");
+            menuItems[3].classList.toggle("menu--top");
+            menuItems[4].classList.toggle("menu--center");
+            menuItems[5].classList.toggle("menu--bottom");
+            nav.classList.toggle("active");
+        });
+    });
 }
-
-
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,19 +15,19 @@
 <main>
 
 
-    <nav class="m-nav">
+    <nav class="nav">
         <div class="px-4 md:px-0 md:container">
-            <div class="m-nav__wrapper">
-        <div class="m-nav__logo mr-5">Logo</div>
-        <div class="m-nav__bar">
-        <ul class="m-nav__list mr-auto">
-            <li class="m-nav__item"><a href="" class="m-nav__link">home</a></li>
-            <li class="m-nav__item"><a href="" class="m-nav__link">Goal</a></li>
-            <li class="m-nav__item"><a href="" class="m-nav__link">functions</a></li>
-            <li class="m-nav__item"><a href="" class="m-nav__link">pricing</a></li>
-            <li class="m-nav__item"><a href="" class="m-nav__link">contact me</a></li>
+            <div class="nav__wrapper">
+        <div class="nav__logo mr-7">Logo</div>
 
-        </ul><div class="m-nav__buttons"><x-button class="mr-3" variant="primary" size="md">sign up</x-button><x-button variant="outline" size="md">login</x-button></div></div>
+        <ul class="nav__list mr-auto hidden lg:flex">
+            <li class="nav__item"><a href="" class="nav__link">home</a></li>
+            <li class="nav__item"><a href="" class="nav__link">Goal</a></li>
+            <li class="nav__item"><a href="" class="nav__link">functions</a></li>
+            <li class="nav__item"><a href="" class="nav__link">pricing</a></li>
+            <li class="nav__item"><a href="" class="nav__link">contact me</a></li>
+
+        </ul><div class="nav__buttons  hidden lg:block"><x-button class="mr-3" variant="primary" size="md">sign up</x-button><x-button variant="outline" size="md">login</x-button></div>
     <button class="menu">
         <div class="menu__item"></div>
         <div class="menu__item"></div>
@@ -35,6 +35,27 @@
     </button>
 </div></div>
     </nav>
+
+    <div class="m-nav">
+        <div class="px-4 md:px-0 md:container">
+        <div class="m-nav__header">
+            <div class="nav__logo mr-7">Logo</div>
+            <button class="menu">
+                <div class="menu__item"></div>
+                <div class="menu__item"></div>
+                <div class="menu__item"></div>
+            </button>
+        </div>
+        <ul class="m-nav__list mb-7">
+            <li class="m-nav__item"><a href="" class="m-nav__link">home</a></li>
+            <li class="m-nav__item"><a href="" class="m-nav__link">goal</a></li>
+            <li class="m-nav__item"><a href="" class="m-nav__link">functions</a></li>
+            <li class="m-nav__item"><a href="" class="m-nav__link">pricing</a></li>
+            <li class="m-nav__item"><a href="" class="m-nav__link">contact me</a></li>
+        </ul>
+        <div class="m-nav__buttons"><x-button class="mr-3" variant="primary" size="md">sign up</x-button><x-button variant="outline" size="md">login</x-button></div>
+        </div>
+    </div>
 
 </main>
     </body>
